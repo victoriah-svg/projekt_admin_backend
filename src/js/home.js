285753d9671733119405a448c1e66bb21bf60401
Victoria Helin <vickanhelin@hotmail.com>
2026-05-28 16:39:09 +0200

    gjort de anrop av printMenu som behövs för att skriva ut meny på startsidan

--- src/js/home.js
+++ src/js/home.js
@@ -6,24 +6,32 @@ import { printMenu } from "./library";
 
 
 
-document.addEventListener("DOMContentLoaded", async() => {
+document.addEventListener("DOMContentLoaded", async () => {
     //ul-element där data för meny ska skrivas ut
     const brunchList = document.getElementById("brunchList");
     const coldDrinkList = document.getElementById("coldDrinkList");
     const hotDrinkList = document.getElementById("hotDrinkList");
     const dessertList = document.getElementById("dessertList");
 
-    
+
     //anropar getFood funktion som importerats från library.js
     let allFood = await getFood();
     //anropar getDrinks funktion som importerats från library.js
     let allDrinks = await getDrinks();
-    //variabel som skickas med som argument i anrop av printFood från library.js
+    //variabler som skickas med som argument i anrop av printFood från library.js
     let brunch = "brunch";
+    let dessert = "dessert";
     let coldDrink = "cold drink";
-    //anropar printFood från library.js med data som hämtats från getFood, samt argument för att skriva ut i brunchlist
-    let callPrintFood = printMenu(allFood, brunch, brunchList, false);
-
-    let callPrintMenu = printMenu(allDrinks, coldDrink, coldDrinkList, false);
+    let hotDrink = "hot drink";
     
+
+    //anropar printFood från library.js med data som hämtats från getFood, samt argument för att skriva ut i brunchlist
+    printMenu(allFood, brunch, brunchList, false);
+    //anropar printFood för dessert
+    printMenu(allFood, dessert, dessertList, false);
+    //anropar printFood för kalla drycker
+    printMenu(allDrinks, coldDrink, coldDrinkList, false);
+    //anropar printFood för varma drycker
+    printMenu(allDrinks, hotDrink, hotDrinkList, false);
+
 });
