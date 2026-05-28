07c22cd01129c57c6eeeb169db0377bb83170f21
Victoria Helin <vickanhelin@hotmail.com>
2026-05-28 16:18:19 +0200

    lagt till funktion som skriver ut food data i DOM i library filen och kallat på den från home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,24 @@
 "use strict";
-import { getFood } from "/src/js/library.js";
-import { getDrinks } from "/src/js/library.js";
+import { getFood, printFood } from "./library";
+import { getDrinks } from "./library";
 
 
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded", async() => {
     //ul-element där data för meny ska skrivas ut
     const brunchList = document.getElementById("brunchList");
     const coldDrinkList = document.getElementById("coldDrinkList");
     const hotDrinkList = document.getElementById("hotDrinkList");
     const dessertList = document.getElementById("dessertList");
 
-    getFood();
-    getDrinks();
-
+    
+    //anropar getFood funktion som importerats från library.js
+    let allFood = await getFood();
+    //anropar getDrinks funktion som importerats från library.js
+    let allDrinks = await getDrinks();
+    //variabel som skickas med som argument i anrop av printFood från library.js
+    let brunch = "brunch";
+    //anropar printFood från library.js med data som hämtats från getFood, samt argument för att skriva ut i brunchlist
+    let callPrintFood = printFood(allFood, brunch, brunchList, true);
+    
 });
--- src/js/library.js
+++ src/js/library.js
@@ -1,4 +1,5 @@
 "use strict";
+/*------ Fil med återanvändbara funktioner ------*/
 
 //hämtar data från food-tabell
 export async function getFood() {
@@ -7,23 +8,58 @@ export async function getFood() {
         let response = await fetch("http://localhost:3000/food");
         //konverterar svaret från json
         let result = await response.json();
-       console.log(result);
+        return result
     } catch (error) {
         console.log(error);
     }
 }
 
 //hämtar data från drink-tabell
-export async function getDrinks(){
+export async function getDrinks() {
 
     try {
         //Hämtar all dryck
         let response = await fetch("http://localhost:3000/drink");
         //konverterar svaret från json
         let result = await response.json();
-        console.log(result);
-        
+        return result;
+
     } catch (error) {
         console.log(error);
     }
+}
+
+//Skriver ut food data i DOM
+export function printFood(foodData, category, foodList, hasDeleteButton) {
+    foodData.forEach(food => {
+        //Om kategori är samma som den som skickats med - skriv ut i den foodList som skickats med
+        if (food.category === category) {
+            //om description finns med , skriv ut den också
+            if (food.description) {
+                foodList.innerHTML += `
+                <li>
+                    <span class="item">${food.name}</span>
+                    <span class="price">${food.price}:-</span>
+                    <span class="description">${food.description}</span>
+                </li>`;
+
+            } else {
+                //om description inte finns med - skriv bara ut name och price
+                foodList.innerHTML += `
+                <li>
+                    <span class="item">${food.name}</span>
+                    <span class="price">${food.price}:-</span>
+                </li>`;
+            }
+            //om deletebtn = true så skriv ut knappar för delete och update 
+            if (hasDeleteButton) {
+                foodList.innerHTML += `
+            <i class="fa-solid fa-trash"></i><button class="${food._id}">Delete</button>
+            <i class="fa-solid fa-pen"></i><button class="${food._id}">Update</button>
+            `;
+
+            }
+        }
+
+    });
 }
