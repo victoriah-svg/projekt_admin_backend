974858b40105a73b5dc50acef75683f7f80c5f43
Victoria Helin <vickanhelin@hotmail.com>
2026-05-28 20:20:08 +0200

    gjort funktion för att hämta och skriva ut meddelanden

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,22 +1,41 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", async()=>{
-   // getMessages();
-   console.log("Hej från messages");
-    
+document.addEventListener("DOMContentLoaded", async () => {
+    getMessages();
+
 });
 
-/*async function getMessages() {
+async function getMessages() {
+    //hämta in token 
+    const token = localStorage.getItem("cv_token");
+
+    //section för meddelanden
     const messageSection = document.getElementById("messageSection");
-    
+
     try {
-        //Hämtar alla maträtter
-        let response = await fetch("http://localhost:3000/message");
-        //konverterar svaret från json
-        let result = await response.json();
-        return result
+        //hämtar alla meddelanden och skickar med token i anrop
+        const response = await fetch('http://localhost:3000/message', {
+            method: "GET",
+            headers: {
+                "authorization": "Bearer " + token,
+                "content-type": "application/json"
+
+            }
+        });
+        //lagrar resultatet
+        const result = await response.json();
+        //loopar igenom array med result och skriver ut till DOM
+        result.forEach(message => {
+            messageSection.innerHTML += `
+            <article>
+                <p><strong>Name: </strong>${message.name}</p><p><strong>Email: </strong>${message.email}</p>
+                <p>${message.message}</p>
+            </article>`;
+        });
+
+
     } catch (error) {
-        console.log(error);
+        console.log("Something went wrong " + error);
     }
-    
-}*/
+
+}
